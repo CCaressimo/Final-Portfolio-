b443c942b9d1dbbac3380b855512f3457690fcd6
chris <c.caressimo@gmail.com>
2021-05-29 19:29:17 -0400

    removed sanity, basis of project layout

--- final-portfolio/src/components/Home.js
+++ final-portfolio/src/components/Home.js
@@ -3,12 +3,20 @@ import image from "../images/niflheim-1080P-wallpaper.jpg";
 
 export default function Home() {
     return (
-        <main>
-            <img src={image} alt="niflheim" className="fixed object-cover w-full h-full"/>
-            <section>
-                <h1>Christopher Caressimo</h1>
-                <h3>Full Stack Web Developer//Musician</h3>
-            </section>
-        </main>
-    )
+      <main>
+        <img
+          src={image}
+          alt="niflheim"
+          className="absolute object-cover w-full h-full"
+        />
+        <section className="relative justify-center min-h-screen pt-12 lg:pt-64 px-8">
+          <h1 className="text-6xl text-yellow-700 leading-none lg:leading-snug">
+            Christopher Caressimo
+          </h1>
+          <h3 className="text-2xl text-yellow-500 leading-none lg:leading-snug">
+            Full Stack Web Developer//Musician
+          </h3>
+        </section>
+      </main>
+    );
 }
--- final-portfolio/src/components/Projects.js
+++ final-portfolio/src/components/Projects.js
@@ -1,5 +1,34 @@
 import React from "react";
 
+
 export default function Projects() {
-  return <h1>Projects Page</h1>;
+
+  return (
+    <main className="bg-gray-200 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex text-black justify-center">Projects</h1>
+        <h3 className="text-lg text-black flex justify-center mb-12">
+          Come check out some of the projects I have built during my time in
+          DigitalCrafts! There will be links for both the live site and Github
+          repository.
+        </h3>
+        <section className="grid grid-cols-2 gap-8">
+          <article className="relative rounded-lg shadow-xl bg-black p-16">
+            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-yellow-500">
+              <a href="" alt="" target="_blank" rel="noopener noreferrer">
+                
+              </a>
+            </h3>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+              <p></p>
+              <span></span>
+            </div>
+          </article>
+        </section>
+      </section>
+    </main>
+  );
 }
