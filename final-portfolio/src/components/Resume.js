dbd647b936536fae2f4994921a41ee06bb9d134e
chris <c.caressimo@gmail.com>
2021-06-17 21:15:48 -0400

    added download button

--- final-portfolio/src/components/Resume.js
+++ final-portfolio/src/components/Resume.js
@@ -20,12 +20,34 @@ export default function Resume() {
           <h1>this is my resume</h1>
 
           <div class="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-            <div class="flex flex-col w-full overflow-y-hidden transparent container rounded-t rounded-b px-4">
+            <div class="flex flex-col w-full overflow-y-hidden container rounded-t rounded-b px-4">
+              <div className="text-center py-6">
+                <button class="headerText nextButton border border-gray-200 py-2 px-4 shadow rounded inline-flex items-center">
+                  <svg
+                    class="fill-current w-4 h-4 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                  </svg>
+                  <a
+                    href="#"
+                    target="_blank"
+                  >
+                    Download
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+            <div class="flex flex-col w-full overflow-y-hidden container rounded-t rounded-b px-4">
               <nav>
                 <div className="text-center py-6">
                   <NavLink
                     to="/contact"
-                    class="headerText nextButton text-2xl py-2 px-4 border border-gray-200 rounded shadow "
+                    className="headerText nextButton text-2xl py-2 px-4 border border-gray-200 rounded shadow"
                   >
                     Contact
                   </NavLink>
